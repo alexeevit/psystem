f59ca102b7fab516c8d790dacae6c6638d3db2ce
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2021-03-19 22:17:34 +0300

    Feature: add seeds and fix admin main page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(email: "admin@example.com", password: "password")
-Merchant.create(email: "merchant@example.com", password: "password")
+Admin.create!(email: "admin@example.com", password: "password")
+Merchant.create!(email: "merchant@example.com", password: "password", status: :active, name: "Mike")
